a18f28152db43c0c7608390a91f96810176ca2aa
karina611 <56723482+karina611@users.noreply.github.com>
2019-11-14 19:23:54 +0100

    Update index.js

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -5,5 +5,18 @@
  * @returns {number}
  */
 function pow(m, n) {
+  let result = m;
+	for (let x = 1; x < n; x++) {
+		result *= m;
+	}
+	return result;
+
+	};
+
+	let first_number = prompt ('Введите число', '');
+	let second_number = prompt ('Введите его степень', '');
+	
+	alert("Ответ: " + pow(first_number, second_number));
+	
 
 }
